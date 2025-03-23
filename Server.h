b3d4d52fefcc782aa3470e042c7a8e176a5b7f71
Hector A. Velez Fraguada <158231689+HectorAVelezFraguada@users.noreply.github.com>
2025-03-23 12:36:33 -0700

    Create Server.h

---
+++ Server.h
@@ -0,0 +1,23 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 1024
+#define TOTAL_CONNECTIONS 10
+
+typedef struct sockaddr_in sockaddr_in;
+
+typedef struct Server {
+    int server_socket;
+    int client_socket;
+    int flag;
+    sockaddr_in svr_addr;
+    sockaddr_in conn_addr;
+} Server;
+
+Server setSocketAddr(int port, const char ip[]);
+int activate_server(sockaddr_in s_addr);
+void communicate_with_client(int server_socket);
+
+#endif // SERVER_H
