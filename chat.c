7c42641b3dd198fafbbb914430cfaf13e7e77dc7
Hector A. Velez Fraguada <158231689+HectorAVelezFraguada@users.noreply.github.com>
2025-03-23 12:08:53 -0700

    Update chat.c

--- chat.c
+++ chat.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <regex.h>
-#include "Command_handler.c"
-#include "Server.c"
-#include "Client.c"
-
+#include "Server.h"
+#include "Client.h"
+#include "Command_handler.h"
 
 int main(int argc, char* argv[]) {
-    // check for missing user input on command interface
-    if ((argv[1] == NULL) || (argv[2] == NULL)) {
-        fprintf(stderr, "\nMissing port number and/or IP address.\nPlease enter in form: ./[*Program exec*] [*Port number*] [*IP Address*]\n\n");
+    if (argc < 3) {
+        fprintf(stderr, "\nMissing port number and/or IP address.\nPlease enter in form: ./program [Port] [IP]\n\n");
         return -1;
     }
 
     Server s;
     int port = atoi(argv[1]);
-    const char* ip = argv[2]; 
+    const char* ip = argv[2];
     s = setSocketAddr(s, port, ip);
-    // check if port number and ip are valid
-    if (s.flag > 0) { 
-        fprintf(stderr, "Port number and/or IP address invalid.\nUnable to connect.\n\n");
+
+    if (s.flag > 0) {
+        fprintf(stderr, "Invalid port number and/or IP address.\n");
         return s.flag;
     }
-    // prepare socket for binding to server and listening to connections
+
     s.server_socket = activate_server(s.server_socket, s.svr_addr);
-    char* input;
-    while (input != "exit") {
+
+    char input[256];
+    while (fgets(input, sizeof(input), stdin)) {
+        if (strncmp(input, "exit", 4) == 0) break;
         execute_command(input);
-    } 
+    }
 
-    //communicate_with_client(s.server_socket, s.client_socket, s.svr_addr, s.conn_addr);
-
-    // close all open sockets
     printf("\nClosing sockets...\n");
     close(s.server_socket);
     close(s.client_socket);
     printf("Successfully disconnected.\n\n");
+
     return 0;
 }
